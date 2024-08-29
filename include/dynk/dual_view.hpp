d43443e02617846625c46a3266b7d8272258e5f0
Paul Zehner <paul.zehner@cea.fr>
2024-08-29 14:01:47 +0200

    Separate dual view functions

---
+++ include/dynk/dual_view.hpp
@@ -0,0 +1,83 @@
+#ifndef __DUAL_VIEW_HPP__
+#define __DUAL_VIEW_HPP__
+
+#include <Kokkos_Core.hpp>
+
+namespace dynk {
+
+/**
+ * Get a View of a DualView for the requested memory space.
+ *
+ * The DualView memory spaces should match with the ones expected. This
+ * function is just a pass-through to manipulate the DualView with a simpler
+ * syntax.
+ *
+ * @tparam MemorySpace Memory space requested.
+ * @tparam DualView Type of the DualView.
+ * @param dualView DualView to take a view from.
+ * @return View in the requested memory space.
+ */
+template <typename MemorySpace, typename DualView>
+auto getView(DualView &dualView) {
+  return dualView.template view<MemorySpace>();
+}
+
+/**
+ * Get a View of a DualView for the requested memory space which is
+ * synchronized if needed.
+ *
+ * The DualView memory spaces should match with the ones expected. This
+ * function is just a pass-through to manipulate the DualView with a simpler
+ * syntax.
+ *
+ * @tparam MemorySpace Memory space requested.
+ * @tparam DualView Type of the DualView.
+ * @param dualView DualView to take a view from.
+ * @return View in the requested memory space, synchronized.
+ */
+template <typename MemorySpace, typename DualView>
+auto getSyncedView(DualView &dualView) {
+  dualView.template sync<MemorySpace>();
+  return getView<MemorySpace>(dualView);
+}
+
+/**
+ * Mark a DualView as modified in the requested memory space.
+ *
+ * The DualView memory spaces should match with the ones expected. This
+ * function is just a pass-through to manipulate the DualView with a simpler
+ * syntax.
+ *
+ * @tparam MemorySpace Memory space requested.
+ * @tparam DualView Type of the DualView.
+ * @param dualView DualView to set.
+ */
+template <typename MemorySpace, typename DualView>
+void setModified(DualView &dualView) {
+  dualView.template modify<MemorySpace>();
+}
+
+/**
+ * Mark a DualView as modified in the requested memory space dynamically.
+ *
+ * @tparam DualView Type of the DualView.
+ * @tparam DeviceMemorySpace Device memory space. Defaults to Kokkos default
+ * execution space's default memory.
+ * @tparam HostMemorySpace Host memory space. Defaults to Kokkos default host
+ * execution space's default memory.
+ * @param dualView DualView to set.
+ * @param isExecutedOnDevice If `true`, mark device view as modified, otherwise
+ * mark host view as modified.
+ */
+template <typename DualView, typename DeviceMemorySpace = Kokkos::DefaultExecutionSpace::memory_space, typename HostMemorySpace = Kokkos::DefaultHostExecutionSpace::memory_space>
+void setModified(DualView &dualView, bool const isExecutedOnDevice) {
+  if (isExecutedOnDevice) {
+    setModified<DeviceMemorySpace>(dualView);
+  } else {
+    setModified<HostMemorySpace>(dualView);
+  }
+}
+
+} // namespace dynk
+
+#endif // ifndef __DUAL_VIEW_HPP__
--- include/dynk/wrapper.hpp
+++ include/dynk/wrapper.hpp
@@ -3,10 +3,13 @@
 
 #include <Kokkos_Core.hpp>
 
+#include "dynk/dual_view.hpp"
+
 namespace dynk {
 
 /**
- * Allow to dynamically launch a parallel block region on device or on host.
+ * Allow to dynamically launch a parallel block region on device or on host by
+ * giving a templated launcher.
  *
  * @tparam ParallelLauncher Type of the functor.
  * @tparam DeviceExecutionSpace Kokkos execution space for device execution,
@@ -44,20 +47,17 @@ void wrap(bool const isExecutedOnDevice,
 }
 
 /**
- * Allow to dynamically launch a parallel block region on device or on host.
+ * Allow to dynamically launch a parallel block region on device or on host by
+ * giving two specialized launchers.
  *
- * @tparam ParallelLauncher Type of the functor.
- * @tparam DeviceExecutionSpace Kokkos execution space for device execution,
- * defaults to Kokkos default execution space.
- * @tparam DeviceMemorySpace Kokkos memory space for device memory, defaults to
- * Kokkos default execution space's default memory space.
- * @tparam HostExecutionSpace Kokkos execution space for host execution,
- * defaults to Kokkos default host execution space.
- * @tparam HostMemorySpace Kokkos memory space for host memory, defaults to
- * Kokkos default host execution space's default memory space.
+ * @tparam ParallelLauncherDevice Type of the device launcher functor.
+ * @tparam ParallelLauncherHost Type of the host launcher functor.
  * @param isExecutedOnDevice If `true`, the parallel block region is launched
  * for execution on the device, otherwise on the host.
- * @param parallelLauncher Functor to launch that contains a parallel block region.
+ * @param parallelLauncherDevice Device launcher functor that contains a
+ * parallel block region.
+ * @param parallelLauncherHost Host launcher functor that contains a parallel
+ * block region.
  */
 template <typename ParallelLauncherDevice,
           typename ParallelLauncherHost>
@@ -72,39 +72,6 @@ void wrap(bool const isExecutedOnDevice,
     // launch for host execution
     parallelLauncherHost();
   }
-}
-
-/**
- * Get a View of a DualView for the requested memory space.
- *
- * The DualView memory spaces should match with the ones expected. This
- * function is just a pass-through to manipulate the DualView with a simpler
- * syntax.
- *
- * @tparam MemorySpace Memory space requested.
- * @tparam DualView Type of the DualView.
- * @param dualView DualView to take a view from.
- * @return View in the requested memory space.
- */
-template <typename MemorySpace, typename DualView>
-auto getView(DualView &dualView) {
-  return dualView.template view<MemorySpace>();
-}
-
-/**
- * Set that a DualView was modified in the requested memory space.
- *
- * The DualView memory spaces should match with the ones expected. This
- * function is just a pass-through to manipulate the DualView with a simpler
- * syntax.
- *
- * @tparam MemorySpace Memory space requested.
- * @tparam DualView Type of the DualView.
- * @param dualView DualView to set.
- */
-template <typename MemorySpace, typename DualView>
-void setModified(DualView &dualView) {
-  dualView.template modify<MemorySpace>();
 }
 
 } // namespace dynk
